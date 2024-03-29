c7dae1e8c68bc0b41f7313bc8f013bc9074e2323
Ayush Kumar <github@ayukmr.com>
2024-03-28 21:33:43 -0700

    Remove monospaced digits

--- Tracks/TrainView.swift
+++ Tracks/TrainView.swift
@@ -67,7 +67,7 @@ struct TrainView: View {
                             Text(
                                 data.0.expected
                                     .formatted(date: .omitted, time: .shortened)
-                            ).monospacedDigit()
+                            )
                         }
                         .gridColumnAlignment(.trailing)
                     }
