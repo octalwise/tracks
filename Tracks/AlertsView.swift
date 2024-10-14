daf2524d419cd09ae49066e8bf6748bc0a70a253
Ayush Kumar <github@ayukmr.com>
2024-10-13 18:19:42 -0700

    Update alerts view

--- Tracks/AlertsView.swift
+++ Tracks/AlertsView.swift
@@ -6,7 +6,7 @@ struct AlertsView: View {
     let alerts: [Alert]
 
     var body: some View {
-        Grid {
+        VStack {
             ForEach(
                 Array(self.alerts.enumerated()),
                 id: \.1.self
@@ -15,31 +15,55 @@ struct AlertsView: View {
                     Divider()
                 }
 
-                GridRow {
-                    HStack {
-                        // trimmed header
-                        DisclosureGroup(alert.header) {
-                            HStack {
-                                // expanded description
-                                VStack {
-                                    Text(alert.header)
-
-                                    if alert.description != nil {
-                                        Text(alert.description!)
-                                    }
-                                }
-                                .lineLimit(nil)
-
-                                Spacer()
-                            }
-                        }
-                        .lineLimit(1)
-                        .multilineTextAlignment(.leading)
-                    }.gridColumnAlignment(.leading)
-                }.padding([.leading, .trailing], 15)
+                AlertItem(alert: alert)
             }
         }
         .padding(.top, 10)
-        .padding(.bottom, 15)
+        .padding([.bottom, .leading, .trailing], 15)
+    }
+}
+
+// alert item
+struct AlertItem: View {
+    // alert
+    let alert: Alert
+
+    @State var expanded = false
+
+    var body: some View {
+        VStack {
+            Button(action: {
+                withAnimation(.none) {
+                    // toggle multiple lines
+                    expanded = !expanded
+                }
+            }) {
+                // header
+                Text(alert.header)
+                    .lineLimit(expanded ? nil : 1)
+                    .multilineTextAlignment(.leading)
+
+                Spacer()
+
+                VStack {
+                    Image(systemName:  "chevron.right")
+                        .rotationEffect(.degrees(expanded ? 90 : 0))
+                        .animation(.linear(duration: 0.25), value: expanded)
+                        .offset(y: 2)
+
+                    Spacer()
+                }
+            }
+
+            if alert.description != nil && expanded {
+                // description
+                HStack {
+                    Text(alert.description!)
+                        .multilineTextAlignment(.leading)
+
+                    Spacer()
+                }
+            }
+        }.padding([.top, .bottom], 5)
     }
 }
